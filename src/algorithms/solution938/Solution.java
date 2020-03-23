14935f3e90f6ba992fa028268ff42a552e7815d9
xiaohaoxing <xiaohaoxing@outlook.com>
2020-03-23 14:16:07 +0800

    ✅  AC 938题。
    Signed-off-by: 肖皓星 <xiaohaoxing@xiaomi.com>

---
+++ src/algorithms/solution938/Solution.java
@@ -0,0 +1,16 @@
+package algorithms.solution938;
+
+/**
+ * @author 肖皓星
+ * @email xiaohaoxing@outlook.com
+ */
+public class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return 0;
+        }
+        return (root.val >= L && root.val <= R ? root.val : 0) + (root.left == null ? 0 : rangeSumBST(root.left, L, R)) + (root.right == null ? 0 : rangeSumBST(root.right, L, R));
+    }
+}
+
+
