158166d9b6a723bc9433212304d7509aba2ba0e6
xiaohaoxing <xiaohaoxing@outlook.com>
2020-03-27 22:04:49 +0800

    ✅  AC 617题。
    Signed-off-by: xiaohaoxing <xiaohaoxing@outlook.com>

---
+++ src/algorithms/solution617/Solution.java
@@ -0,0 +1,23 @@
+package algorithms.solution617;
+
+/**
+ * @author 肖皓星
+ * @email xiaohaoxing@outlook.com
+ */
+public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null) {
+            return null;
+        }
+        if(t1 == null) {
+            return t2;
+        }
+        if(t2 == null) {
+            return t1;
+        }
+        TreeNode node = new TreeNode(t1.val + t2.val);
+        node.left = mergeTrees(t1.left, t2.left);
+        node.right = mergeTrees(t1.right, t2.right);
+        return node;
+    }
+}
---
+++ src/algorithms/solution617/TreeNode.java
@@ -0,0 +1,15 @@
+package algorithms.solution617;
+
+/**
+ * @author 肖皓星
+ * @email xiaohaoxing@outlook.com
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
