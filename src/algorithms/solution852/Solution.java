1520c0da8e1438ed3e15bc54006427912ada9883
xiaohaoxing <xiaohaoxing@outlook.com>
2020-04-12 00:56:07 +0800

    ✅  AC 852.
    Signed-off-by: xiaohaoxing <xiaohaoxing@outlook.com>

---
+++ src/algorithms/solution852/Solution.java
@@ -0,0 +1,14 @@
+package algorithms.solution852;
+
+/**
+ * @author 肖皓星
+ * @email xiaohaoxing@outlook.com
+ */
+public class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        for (int i = 0; i < A.length - 1; i++) {
+            if(A[i] > A[i+1]) return i;
+        }
+        return 0;
+    }
+}
