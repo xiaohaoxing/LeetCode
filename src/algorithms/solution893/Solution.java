40c3ddf38537d44c28286b924c674bef2d99039e
肖皓星 <xiaohaoxing@xiaomi.com>
2020-09-16 19:57:59 +0800

    ✅ AC 893
    Signed-off-by: xiaohaoxing <xiaohaoxing@outlook.com>

--- src/algorithms/solution893/Solution.java
+++ src/algorithms/solution893/Solution.java
@@ -1,7 +1,9 @@
 package algorithms.solution893;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author 肖皓星
@@ -9,12 +11,53 @@ import java.util.List;
  */
 public class Solution {
     public int numSpecialEquivGroups(String[] A) {
-        return 0;
+        List<CharSetPair> uniqueList = new ArrayList<>();
+        for (String a : A) {
+            // construct the sets
+            char[] chars = a.toCharArray();
+            CharSetPair pair = new CharSetPair();
+            for (int i = 0; i < chars.length; i++) {
+                if (i % 2 == 0) {
+                    pair.evenSet.put(chars[i], pair.evenSet.getOrDefault(chars[i], 0) + 1);
+                } else {
+                    pair.oddSet.put(chars[i], pair.oddSet.getOrDefault(chars[i], 0) + 1);
+                }
+            }
+            if (!uniqueList.contains(pair)) {
+                uniqueList.add(pair);
+            }
+        }
+        return uniqueList.size();
     }
 
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Alice", "Bob", "Chary");
-        String result = names.stream().reduce("", (a, b) -> a.isEmpty() ? b : a + "," + b);
-        System.out.println(result);
+        String[] in = new String[]{"abcd", "cdab", "cbad", "xyzz", "zzxy", "zzyx"};
+        String[] in2 = new String[]{"ababaa", "aaabaa"};
+        Solution test = new Solution();
+        System.out.println(test.numSpecialEquivGroups(in));
+    }
+}
+
+class CharSetPair {
+    HashMap<Character, Integer> oddSet;
+    HashMap<Character, Integer> evenSet;
+
+    public CharSetPair() {
+        oddSet = new HashMap<>();
+        evenSet = new HashMap<>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CharSetPair that = (CharSetPair) o;
+        return Objects.equals(oddSet, that.oddSet) &&
+                Objects.equals(evenSet, that.evenSet);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(oddSet, evenSet);
     }
 }
