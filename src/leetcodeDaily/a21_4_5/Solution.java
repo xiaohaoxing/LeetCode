595a8fcbf70b3c4b122d2432047d9c954c729b16
xiaohaoxing <xiaohaoxing@outlook.com>
2021-04-05 17:22:37 +0800

    完成 20-04-05 daily

--- src/leetcodeDaily/a21_4_5/Solution.java
+++ src/leetcodeDaily/a21_4_5/Solution.java
@@ -1,5 +1,23 @@
-class Solution {
+package leetcodeDaily.a21_4_5;
+
+public class Solution {
     public boolean isIdealPermutation(int[] A) {
-        
+        if (A.length <= 2) {
+            return true;
+        }
+        for (int i = 0; i < A.length - 2; i++) {
+            for (int j = i + 2; j < A.length; j++) {
+                if (A[i] > A[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] A = new int[]{1, 2, 0};
+        Solution test = new Solution();
+        System.out.println(test.isIdealPermutation(A));
     }
 }
