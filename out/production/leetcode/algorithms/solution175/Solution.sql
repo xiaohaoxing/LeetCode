1963c7d7df40c55561ba18f052ac2e65864aeee3
xiaohaoxing <xiaohaoxing@outlook.com>
2024-02-29 20:37:20 +0800

    some sql solutions.

--- out/production/leetcode/algorithms/solution175/Solution.sql
+++
@@ -1,3 +0,0 @@
-select p.FirstName, p.LastName, a.City, a.State
-from Person as p
-         left join Address as a on p.personId = a.PersonId;
--- out/production/leetcode/algorithms/solution182/Solution.sql
+++
@@ -1,5 +0,0 @@
-select distinct p1.Email
-from Person p1,
-     Person p2
-where p1.Email = p2.Email
-  and p1.Id != p2.Id
