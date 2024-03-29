fe7670cb96afeedd28edafd44e0d23c6b52b0453
xiaohaoxing <xiaohaoxing@outlook.com>
2024-03-29 15:54:03 +0800

    DP1: Solve problem 300.

--- src/algorithms/solution300/Solution.java
+++ src/algorithms/solution300/Solution.java
@@ -20,11 +20,45 @@ public class Solution {
         return res;
     }
 
+    /**
+     * 贪心+二分
+     * 核心问题是：为什么新扫到的数字替换掉结果中指定位置的最小数字？
+     * 答案：因为扫描是从前向后，因此遇到新的合法数字后，现有长度不会丢失，只是替换掉中途的数了，当新的序列长度超过当前的结果时才会增加 len，使结果更新。
+     * 举例：2, 5, 6, 3, 7 这几个数字，先得到 2,5,6这几个序列，再遇到 3，将 5 替换成 3，然后继续到 7，在 6 后面追加 7.得到最终结果长度：4。
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS2(int[] nums) {
+        int n = nums.length;
+        int[] d = new int[n + 1];
+        int len = 1;
+        d[len] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int left = 1, right = len + 1, mid;
+                while (left < right) {
+                    mid = left + (right - left) / 2;
+                    if (d[mid] < nums[i]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+                d[left] = nums[i];
+            }
+        }
+        return len;
+    }
+
+
     public static void main(String[] args) {
 //        int[] nums = new int[]{0, 1, 0, 3, 2, 3,};
-        int[] nums = new int[]{7, 7, 7, 7, 7, 7, 7};
+//        int[] nums = new int[]{7, 7, 7, 7, 7, 7, 7};
+        int[] nums = new int[]{10, 9, 2, 5, 6, 3, 7, 101, 102};
         Solution test = new Solution();
-        int result = test.lengthOfLIS(nums);
+        int result = test.lengthOfLIS2(nums);
         System.out.println(result);
     }
 }
