d8c4389a4f968a0bfe40b3e3d8f3c08e317181f3
xiaohaoxing <xiaohaoxing@outlook.com>
2024-03-29 18:12:07 +0800

    DP4: Solve problem 97.

--- src/algorithms/solution97/Solution.java
+++ src/algorithms/solution97/Solution.java
@@ -12,18 +12,15 @@ public class Solution {
         if (s2.isEmpty()) {
             return s1.equals(s3);
         }
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
         boolean[][] dp = new boolean[s1.length() + 1][];
         for (int i = 0; i <= s1.length(); i++) {
             dp[i] = new boolean[s2.length() + 1];
         }
         // 初始化
         dp[0][0] = true;
-//        for (int i = 1; i < s1.length(); i++) {
-//            dp[i][0] = dp[i - 1][0] && s2.charAt(i) == s3.charAt(i);
-//        }
-//        for (int i = 1; i <= s2.length(); i++) {
-//            dp[0][i] = dp[0][i - 1] && s1.charAt(i) == s3.charAt(i);
-//        }
         // dp
         for (int i = 0; i <= s1.length(); i++) {
             for (int j = 0; j <= s2.length(); j++) {
